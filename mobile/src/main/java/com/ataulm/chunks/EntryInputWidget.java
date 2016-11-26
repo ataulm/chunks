6068bb94068099cad38ae99e485f53608538335c
Ataul Munim <hello@ataulm.com>
2016-11-26 23:28:21 +0000

    Add key and enter listener to InputWidget

--- mobile/src/main/java/com/ataulm/chunks/EntryInputWidget.java
+++ mobile/src/main/java/com/ataulm/chunks/EntryInputWidget.java
@@ -2,10 +2,13 @@ package com.ataulm.chunks;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,19 +33,56 @@ public class EntryInputWidget extends LinearLayout {
         ButterKnife.bind(this);
     }
 
-    public void bind(final EntryInputUserInteractions entryInputUserInteractions, final Day day) {
+    public void bind(final EntryInputUserInteractions userInteractions, final Day day) {
+        setEnterKeyListenerToAddEntryAndPreventMultilineInput(userInteractions, day);
+
         addButton.setOnClickListener(
                 new OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        String entry = inputEditText.getText().toString();
-                        if (entry.length() > 0) {
-                            entryInputUserInteractions.onUserAddEntry(entry, day);
-                            inputEditText.setText(null);
-                        }
+                        addStack(userInteractions, day);
                     }
                 }
         );
+    }
+
+    private void setEnterKeyListenerToAddEntryAndPreventMultilineInput(final EntryInputUserInteractions userInteractions, final Day day) {
+        inputEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (enterOrDone(actionId)) {
+                    addStack(userInteractions, day);
+                    return true;
+                }
+                return false;
+            }
+
+            private boolean enterOrDone(int actionId) {
+                return actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL;
+            }
+        });
+
+        inputEditText.setOnKeyListener(new OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                boolean enterPressed = keyCode == KeyEvent.KEYCODE_ENTER;
+
+                if (enterPressed) {
+                    addStack(userInteractions, day);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void addStack(EntryInputUserInteractions userInteractions, Day day) {
+        String entry = inputEditText.getText().toString();
+        if (entry.length() > 0) {
+            userInteractions.onUserAddEntry(entry, day);
+            inputEditText.setText(null);
+        }
     }
 
 }
