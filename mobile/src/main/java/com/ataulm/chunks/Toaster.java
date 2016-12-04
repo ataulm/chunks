6cd4b58aeb81c870a9bac70a90af7b8e2d8f531a
Ataul Munim <hello@ataulm.com>
2016-12-04 12:15:13 +0000

    Suppress lint for static field assignment of context

--- mobile/src/main/java/com/ataulm/chunks/Toaster.java
+++ mobile/src/main/java/com/ataulm/chunks/Toaster.java
@@ -1,10 +1,12 @@
 package com.ataulm.chunks;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.Toast;
 
 public class Toaster {
 
+    @SuppressLint("StaticFieldLeak") // application context so no activity leak & don't care about instant run
     private static Context context;
     private static Toast toast;
 
