b14bef20c1a74aa7502ab1251af14f3f4ab465f8
Ataul Munim <hello@ataulm.com>
2016-12-04 12:10:57 +0000

    Change BaseActivity to be abstract - should not be used directly

--- mobile/src/main/java/com/ataulm/chunks/BaseActivity.java
+++ mobile/src/main/java/com/ataulm/chunks/BaseActivity.java
@@ -2,5 +2,5 @@ package com.ataulm.chunks;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 }
