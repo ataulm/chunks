e7f7334f0f2c400ca5898b360e0a8cbc1749521d
Ataul Munim <hello@ataulm.com>
2017-03-05 12:28:58 +0000

    Add tests asserting move action visibility

--- mobile/src/androidTest/java/com/ataulm/chunks/ChunkItemViewActivityTest.java
+++ mobile/src/androidTest/java/com/ataulm/chunks/ChunkItemViewActivityTest.java
@@ -11,7 +11,10 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -25,6 +28,48 @@ public class ChunkItemViewActivityTest {
     @Before
     public void setUp() {
         hitCounter = new HitCounter(activityRule.getActivity().getResources());
+    }
+
+    @Test
+    public void givenEntryForToday_moveRightDisplayed() {
+        bind(Day.TODAY, completeEntry());
+
+        onView(withId(R.id.entry_button_move_right)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void givenEntryForToday_moveLeftNotDisplayed() {
+        bind(Day.TODAY, completeEntry());
+
+        onView(withId(R.id.entry_button_move_left)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void givenEntryForTomorrow_moveLeftDisplayed() {
+        bind(Day.TOMORROW, completeEntry());
+
+        onView(withId(R.id.entry_button_move_left)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void givenEntryForTomorrow_moveRightNotDisplayed() {
+        bind(Day.TOMORROW, completeEntry());
+
+        onView(withId(R.id.entry_button_move_right)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void givenEntryForSometime_moveLeftDisplayed() {
+        bind(Day.SOMETIME, completeEntry());
+
+        onView(withId(R.id.entry_button_move_left)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void givenEntryForSometime_moveRightNotDisplayed() {
+        bind(Day.SOMETIME, completeEntry());
+
+        onView(withId(R.id.entry_button_move_right)).check(matches(not(isDisplayed())));
     }
 
     @Test
