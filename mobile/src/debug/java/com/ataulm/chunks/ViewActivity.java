f499fe1d6e42d67f6bbde4589205802464a21ddd
Ataul Munim <hello@ataulm.com>
2017-03-06 16:09:55 +0000

    Extract method for creating ViewLayoutParams

--- mobile/src/debug/java/com/ataulm/chunks/ViewActivity.java
+++ mobile/src/debug/java/com/ataulm/chunks/ViewActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
-// TODO: move to library
 public class ViewActivity extends Activity {
 
     public static final String EXTRA_LAYOUT_ID = ViewActivity.class.getName() + ".EXTRA_LAYOUT_ID";
@@ -16,12 +15,18 @@ public class ViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int layout = getIntent().getIntExtra(EXTRA_LAYOUT_ID, 0);
+
         view = getLayoutInflater().inflate(layout, null, false);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        ViewGroup.LayoutParams params = createMatchParentParams();
+
         setContentView(view, params);
     }
 
+    private ViewGroup.LayoutParams createMatchParentParams() {
+        return new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
     public View getView() {
-        return ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        return view;
     }
 }
